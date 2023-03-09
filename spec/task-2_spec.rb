edaedefedec310ac2aba16879354b1e6b582a53a
ekuzovlev <bio19@yandex.ru>
2023-03-09 15:50:43 +0300

    prepare & memory_profiler

---
+++ spec/task-2_spec.rb
@@ -0,0 +1,16 @@
+require './task-2.rb'
+
+describe "Perfomance" do
+  describe 'work' do
+    it 'works 20_000 under 6 s' do
+      expect {
+        work('data/data_20_000.txt', disable_gc: false)
+      }.to perform_under(6000).ms
+    end
+
+    # it 'perform power' do
+    #   expect { work('data/data_20_000.txt', disable_gc: false) }.to perform_power
+    # end
+  end
+
+end
